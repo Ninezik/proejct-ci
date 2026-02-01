f2bb31f815629aad3bd04ccb608b6f7d73ce9a0f
zikzani <zikzani@gmail.com>
2026-02-01 13:48:23 +0700

    add simple ci

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from project-ci!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_math_utils.py
@@ -0,0 +1,4 @@
+from src.math_utils import tambah
+
+def test_tambah():
+    assert tambah(2, 3) == 5
